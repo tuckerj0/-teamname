f1568641beebb449f38660f8183f50dd3d87add6
Peter McCloskey <pcm17@pitt.edu>
2017-01-23 16:36:03 -0500

    Update menuBar.java

--- java/menuBar.java
+++ java/menuBar.java
@@ -10,16 +10,6 @@ import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author SneakyPT
- */
 public class menuBar implements ActionListener, ItemListener{
     JPopupMenu popup;
     JMenuItem menuItem;
