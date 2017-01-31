460ab75f30fd6db8424ad15c31ec40a1f712bc1c
Peter McCloskey <pcm17@pitt.edu>
2017-01-31 13:14:20 -0500

    Add files via upload

---
+++ java/finalMenu.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class finalMenu {
+
+    private JFrame menuFrame;
+    private JPanel menu;
+    private JPanel buttonPanel;
+    private JButton newGameBtn;
+    private JButton quitGameBtn;
+    private JLabel text;
+
+    public finalMenu() {
+        createMenu();        
+
+    }
+
+    private void createMenu() {
+        menuFrame = new JFrame();
+        menu = new JPanel();
+        buttonPanel = new JPanel();
+        menuFrame.setSize(400, 200);
+        menuFrame.add(menu);
+        
+        text = new JLabel("Game Over");
+        text.setFont(new Font("Courier", Font.PLAIN, 40));
+        menu.setLayout(new FlowLayout());
+        menu.add(text, BorderLayout.CENTER);  
+        menu.add(buttonPanel, BorderLayout.CENTER);
+        
+        newGameBtn = new JButton("New Game");
+        quitGameBtn = new JButton("Quit Game");
+        buttonPanel.add(newGameBtn);
+        buttonPanel.add(quitGameBtn);
+    }
+
+
+    public void showMenu() {
+        menuFrame.setVisible(true);
+        menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
