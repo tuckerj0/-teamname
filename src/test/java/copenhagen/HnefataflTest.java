6eb60f8f450b7a5e8b5d67fdbcee21e0e056bdf6
gcz3 <gcz3@pitt.edu>
2017-02-01 13:20:43 -0500

    Added a unit test
    just to show we know how it works

---
+++ src/test/java/copenhagen/HnefataflTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import copenhagen.Hnefatafl;
+
+public class HnefataflTest {
+
+    @Test
+    public void testTrue() {
+        assertEquals(true, true);
+    }
+}
